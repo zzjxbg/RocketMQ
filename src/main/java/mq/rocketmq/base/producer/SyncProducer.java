f0a690a9c491f002ccb737751e4297254f6fbafd
zzjxbg <zhaozijie94@gmail.com>
2021-08-28 15:04:49 +0800

    发送同步消息

---
+++ src/main/java/mq/rocketmq/base/producer/SyncProducer.java
@@ -0,0 +1,44 @@
+package mq.rocketmq.base.producer;
+
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.client.producer.SendStatus;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 发送同步消息
+ */
+public class SyncProducer {
+    public static void main(String[] args) throws MQClientException, MQBrokerException, RemotingException, InterruptedException {
+
+        //1.创建消息生产者producer,并制定生产者组名
+        DefaultMQProducer producer = new DefaultMQProducer("group1");
+        //2.指定Nameserver地址
+        producer.setNamesrvAddr("192.168.109.130:9876;192.168.109.132:9876");
+        //3.启动producer
+        producer.start();
+        //4.创建消息对象,指定主题Topic、Tag和消息体
+        for (int i = 0;i < 10;i++) {
+            /**
+             * 参数一：消息主题Topic
+             * 参数二：消息Tag
+             * 参数三：消息内容
+             */
+            Message msg = new Message("base","Tag1",("Hello World" + i).getBytes());
+            //5.发送消息
+            SendResult result = producer.send(msg);
+            //发送状态
+            SendStatus status = result.getSendStatus();
+            System.out.println("发送结果:"+result);
+            //线程睡1秒
+            TimeUnit.SECONDS.sleep(1);
+        }
+        //6.关闭生产者producer
+        producer.shutdown();
+    }
+}
