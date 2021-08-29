95cd456d32451f09d85eb043adebdbe4df1f2446
zzjxbg <zhaozijie94@gmail.com>
2021-08-29 15:40:51 +0800

    Tag过滤

---
+++ src/main/java/mq/rocketmq/filter/tag/Consumer.java
@@ -0,0 +1,37 @@
+package mq.rocketmq.filter.tag;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
+
+import java.util.List;
+
+public class Consumer {
+    public static void main(String[] args) throws MQClientException {
+        // 1.创建消费者Consumer,制定消费者组名
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("group1");
+        // 2.指定Nameserver地址
+        consumer.setNamesrvAddr("192.168.109.130:9876;192.168.109.132:9876");
+        // 3.订阅主题Topic和Tag
+        consumer.subscribe("FilterTagTopic","");
+
+        // 4.设置回调函数,处理消息
+        // MessageListenerConcurrently:并发消费
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+                for(MessageExt msg : msgs) {
+                    System.out.println(new String(msg.getBody()));
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        // 5.启动消费者consumer
+        consumer.start();
+
+    }
+}
